dbcbe27c2e385a8c67f94ee3137c622927b44b13
SaeedAbi <abdary.saeed@gmail.com>
2024-05-27 15:31:09 +0330

    :construction: create routes for Home,Pricing and Product

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import Home from "./pages/home.jsx";
+import Error from "./pages/Error.jsx";
 
 const App = () => {
     return (
         <div>
-            WorldWide
+       <BrowserRouter>
+<Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="product" element={<Product/>} />
+    <Route path="pricing" element={<Pricing/>} />
+    <Route path='*' element={<Error/>}/>
+</Routes>
+       </BrowserRouter>
         </div>
     );
 };
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+          WorldWise
+        </div>
+    );
+};
+
+export default Home;
