3376753c18b80baef8077b66b291590c38b3fe13
SaeedAbi <abdary.saeed@gmail.com>
2024-05-28 13:08:30 +0330

    :lipstick: add route for login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AppLayout from "././pages/AppLayout/AppLayout";
 import Homepage from "./pages/HomePage/Homepage.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import Pricing from "./pages/Product/Pricing.jsx";
+import Login from "./pages/Login/Login.jsx";
 
 const App = () => {
     return (
@@ -13,6 +14,7 @@ const App = () => {
 <Routes>
     <Route path="/" element={<Homepage/>}/>
     <Route path="product" element={<Product/>} />
+    <Route path="login" element={<Login/>} />
     <Route path="pricing" element={<Pricing/>} />
     <Route path="app" element={<AppLayout/>} />
     <Route path='*' element={<PageNotFound/>}/>
