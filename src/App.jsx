bc5c6e8a8eafe069fac8520d1172c1ba517f837f
SaeedAbi <abdary.saeed@gmail.com>
2024-05-29 15:42:45 +0330

    :construction: add City component

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,33 @@ import Homepage from "./pages/HomePage/Homepage.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import Pricing from "./pages/Product/Pricing.jsx";
 import Login from "./pages/Login/Login.jsx";
+import CityList from "./Components/CityList/CityList.jsx";
+import {useEffect, useState} from "react";
+import CountriesList from "./Components/CountriesList/CountriesList.jsx";
+import City from "./Components/City/City.jsx";
+
+const BASE_URL='http://localhost:9000'
 
 const App = () => {
+    const [cities,setCities]=useState([])
+    const [isLoading,setIsLoading]= useState(false)
+
+
+    useEffect(() => {
+       async function fetchCities(){
+          try {
+              setIsLoading(true)
+             const res=await fetch(`${BASE_URL}/cities`)
+             const data=await res.json()
+             setCities(data);
+       }  catch {
+              alert('There was an error')
+       }finally {
+              setIsLoading(false)
+          }
+       }
+fetchCities()
+    }, []);
     return (
         <div>
 
@@ -17,9 +42,10 @@ const App = () => {
     <Route path="login" element={<Login/>} />
     <Route path="pricing" element={<Pricing/>} />
     <Route path="app" element={<AppLayout/>} >
-        <Route index element={<p>List</p>}/>
-         <Route path='cities' element={<p>City</p>}/>
-        <Route path='countries' element={<p>Country</p>}/>
+        <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+         <Route path='cities' element={<CityList cities={cities} isLoading={isLoading}/>}/>
+        <Route path='cities/:id' element={<City/>} />
+        <Route path='countries' element={<CountriesList cities={cities} isLoading={isLoading}/>}/>
         <Route path='form' element={<p>Form</p>}/>
     </Route>
     <Route path='*' element={<PageNotFound/>}/>
