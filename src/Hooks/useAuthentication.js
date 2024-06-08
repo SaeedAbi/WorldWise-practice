ab62a21d0cc8641f78b841c999e78be81ed38e0f
SaeedAbi <abdary.saeed@gmail.com>
2024-06-08 15:30:43 +0330

    :zap: add user component

--- src/Hooks/useAuthentication.js
+++ src/Hooks/useAuthentication.js
@@ -1,8 +1,10 @@
 import {useContext} from "react";
+import {AuthContext} from "../Contexts/FakeAuthContext.jsx";
 
 function useAuth(){
-    const context=useContext(AudioContext)
+    const context=useContext(AuthContext)
     if (context===undefined) throw new Error('AuthContext was used outside AuthProvider ')
+    return context
 }
 
 export default useAuth
